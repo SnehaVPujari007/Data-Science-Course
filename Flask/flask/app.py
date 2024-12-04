2ffcbfe82d73f2f82e97fab97db0c64e62ccf767
Sneha Vilasrao Pujari <154947462+SnehaVPujari007@users.noreply.github.com>
2024-12-04 09:34:07 +0530

    final Commit

---
+++ Flask/flask/app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+'''
+It creates an instance of the Flask class,
+ which will be your WSGI (Web server Gateway Interface) application
+'''
+###  WSGI application
+app = Flask(__name__)
+
+@app.route("/index")
+
+def index():
+    return "Welcome to this best Flask course.This should be an amazing course"
+    
+
+if __name__== "__main__":
+    app.run(debug= True)
